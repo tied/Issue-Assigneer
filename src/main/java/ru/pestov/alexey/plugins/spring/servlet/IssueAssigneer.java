89c5a1f3560834a86e02beefd16666983f26d456
Alexey.Pestov <alexpestov89@yandex.ru>
2021-10-31 18:12:41 +0300

    add path to properties file, begin work for parse json

--- src/main/java/ru/pestov/alexey/plugins/spring/servlet/IssueAssigneer.java
+++ src/main/java/ru/pestov/alexey/plugins/spring/servlet/IssueAssigneer.java
@@ -2,22 +2,30 @@ package ru.pestov.alexey.plugins.spring.servlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.pestov.alexey.plugins.spring.service.HTMLService;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 public class IssueAssigneer extends HttpServlet{
     private static final Logger log = LoggerFactory.getLogger(IssueAssigneer.class);
+
+    FileInputStream fis;
+    Properties property = new Properties();
+
     private String path;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
+
         resp.setContentType("text/html");
-        resp.getWriter().write("<html><body><b>Hello</b> World</body></html>");
+        resp.getWriter().write("<html><body>" + HTMLService.getHTMLCode() +"</body></html>");
     }
 
 }
